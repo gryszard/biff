0400b8060f28f8f62d3a8066e50f96a6a31987d3
Ryszard Gromek <rysiupysiu234@gmail.com>
2024-03-23 14:25:40 +0100

    Add routing and sample primeng button

--- biff/src/app/app.module.ts
+++ biff/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { ButtonModule } from 'primeng/button';
 
 @NgModule({
@@ -9,7 +10,7 @@ import { ButtonModule } from 'primeng/button';
     AppComponent
   ],
   imports: [
-    BrowserModule, ButtonModule
+    BrowserModule, AppRoutingModule, ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
